fc00c32140094dea3f4eebd892b4d837c16df08a
ULU <nilsien2001@gmail.com>
2024-04-28 13:27:19 +0000

    added restart to containers

--- startServer.sh
+++ startServer.sh
@@ -1,4 +1,4 @@
 cargo build --release
 docker build -t ua-fiso:latest .
 docker stop ua-fiso | docker remove ua-fiso
-docker run --network ua-network --name ua-fiso -p 8001:8001 -d ua-fiso:latest
+docker run --restart=always --network ua-network --name ua-fiso -p 8001:8001 -d ua-fiso:latest
