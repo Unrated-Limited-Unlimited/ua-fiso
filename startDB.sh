711a3b723d163a43236b6e535fe8147bc6b5900a
ULU <nilsien2001@gmail.com>
2024-04-28 13:19:14 +0000

    added service and server start scripts

---
+++ startDB.sh
@@ -0,0 +1,2 @@
+docker stop ua-fiso-db | docker remove ua-fiso-db
+docker run --network ua-network --name ua-fiso-db -p 27017:27017 -d mongo --bind_ip_all
--- startServer.sh
+++ startServer.sh
@@ -1,1 +1,4 @@
-cargo build --release && ./target/release/ua-fiso
+cargo build --release
+docker build -t ua-fiso:latest .
+docker stop ua-fiso | docker remove ua-fiso
+docker run --network ua-network --name ua-fiso -p 8001:8001 -d ua-fiso:latest
